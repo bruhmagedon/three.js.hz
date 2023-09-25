6d626d4c35aa5b6299ad1e552dc15e6ca34403fd
bruhmagedon <schubert.walk@yandex.ru>
2023-09-25 18:03:47 +0300

    canvas ready

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import styled from "styled-components";
+import "./app.css";
+
+const App = () => {
+    return (
+        <div className="container">
+            <canvas className="canvas"></canvas>
+        </div>
+    );
+};
+export default App;
