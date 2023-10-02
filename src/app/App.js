4d306ff871c390a9f59832cef167d20f794d15c5
bruhmagedon <schubert.walk@yandex.ru>
2023-10-02 14:23:39 +0300

    faf

--- src/app/App.js
+++ src/app/App.js
@@ -44,11 +44,149 @@ const draw = (ctx, example) => {
                 { x: 450, y: 225 }, //левый вер
             ],
         },
+        HF: {
+            type: "HF",
+            points: [
+                { x: 400, y: 125 }, //левый верх
+                { x: 350, y: 225 }, //левый низ
+                { x: 450, y: 225 }, //левый вер
+            ],
+        },
+        LLF: {
+            type: "LLF",
+            points: [
+                { x: 350, y: 400 }, //левый верх
+                { x: 350, y: 475 }, //левый низ
+                { x: 390, y: 475 }, //левый вер
+                { x: 390, y: 400 }, //левый вер 18
+            ],
+        },
+        RLF: {
+            type: "RLF",
+            points: [
+                { x: 410, y: 400 }, //левый верх 19
+                { x: 410, y: 475 }, //левый низ
+                { x: 450, y: 475 }, //левый вер
+                { x: 450, y: 400 }, //левый вер
+            ],
+        },
+        E1: {
+            type: "E1",
+            points: [
+                { x: 365, y: 240 }, //левый верх
+                { x: 365, y: 260 }, //левый низ
+                { x: 385, y: 260 }, //левый вер
+                { x: 385, y: 240 }, //левый вер
+            ],
+        },
+        E2: {
+            type: "E2",
+            points: [
+                { x: 415, y: 240 }, //левый верх
+                { x: 415, y: 260 }, //левый низ
+                { x: 435, y: 260 }, //левый вер
+                { x: 435, y: 240 }, //левый вер
+            ],
+        },
+        M: {
+            type: "M",
+            points: [
+                { x: 365, y: 270 }, //левый верх
+                { x: 365, y: 290 }, //левый низ
+                { x: 435, y: 290 }, //левый вер
+                { x: 435, y: 270 }, //левый вер
+            ],
+        },
     };
     for (let bodyPart in dwarf) {
         drawBodyPart(ctx, dwarf[bodyPart]);
+        drawBodyPart(ctx, dwarf[bodyPart]);
     }
+
+    scaling(dwarf["BF"]);
+
+    // Определяем объект для масштабирования
+    // var object = {
+    //     x: 100, // начальная координата x
+    //     y: 100, // начальная координата y
+    //     width: 100, // ширина объекта
+    //     height: 100, // высота объекта
+    // };
+
+    // // Определяем матрицу масштабирования
+    // var scaleMatrix = [
+    //     [3, 0, 0], // масштаб по оси x
+    //     [0, 3, 0], // масштаб по оси y
+    //     [0, 0, 1], // не изменяем z координату
+    // ];
+
+    // // Функция для умножения матриц
+
+    // // Функция для применения матрицы масштабирования к объекту
+    // function applyTransformation() {
+    //     console.log("obj", object);
+    //     var transformedMatrix = multiplyMatrix(scaleMatrix, [
+    //         [object.x],
+    //         [object.y],
+    //         [1],
+    //     ]);
+
+    //     console.log("transmatrix", transformedMatrix);
+
+    //     object.x = transformedMatrix[0][0];
+    //     object.y = transformedMatrix[1][0];
+    //     object.width *= scaleMatrix[0][0];
+    //     object.height *= scaleMatrix[1][1];
+    //     console.log("obj2", object);
+    // }
+
+    // // Масштабируем объект
+    // applyTransformation();
+
+    // // Рисуем объект
+    // ctx.fillStyle = "#FF0000"; // красный цвет заливки
+    // ctx.fillRect(object.x, object.y, object.width, object.height);
 };
+
+const scaling = (bodyPart) => {
+    const partMatrix = [];
+    for (let part of bodyPart.points) {
+        const localPart = [part.x, part.y];
+        partMatrix.push(localPart);
+    }
+
+    console.log(partMatrix);
+    const scale = [
+        [2, 0, 0],
+        [0, 2, 0],
+        [0, 0, 1],
+    ];
+    multiplyMatrix(scale, partMatrix);
+    console.log(partMatrix);
+};
+
+function multiplyMatrix(matrix1, matrix2) {
+    console.log("multiply", matrix1, matrix2);
+    var resultMatrix = [];
+
+    for (var i = 0; i < matrix1.length; i++) {
+        var row = [];
+
+        for (var j = 0; j < matrix2[0].length; j++) {
+            var sum = 0;
+
+            for (var k = 0; k < matrix1[0].length; k++) {
+                sum += matrix1[i][k] * matrix2[k][j];
+            }
+
+            row.push(sum);
+        }
+
+        resultMatrix.push(row);
+    }
+
+    return resultMatrix;
+}
 
 //отрисовка части тела
 const drawBodyPart = (ctx, bodyPart) => {
@@ -77,6 +215,7 @@ const App = () => {
                     draw(ctx, canvas);
                 }}
             ></canvas>
+            <button type="button" />
         </div>
     );
 };
